639149033618ac3562778506eeeed22716951218
yanos <yannos@gmail.com>
2026-07-06 08:02:23 -0400

    Show app version in the About Flower dialog
    Reads AssemblyInformationalVersionAttribute off the entry assembly (MinVer-stamped) and strips the commit-sha build metadata before display.

--- Flower/Views/AboutWindow.axaml.cs
+++ Flower/Views/AboutWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Avalonia.Controls;
 
 namespace Flower.Views;
@@ -7,5 +8,19 @@ public partial class AboutWindow : Window
     public AboutWindow()
     {
         InitializeComponent();
+
+        var informationalVersion = Assembly.GetEntryAssembly()?
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+            .InformationalVersion;
+
+        if (informationalVersion != null)
+        {
+            // Strip MinVer's "+<commit-sha>" build metadata - not meaningful to a user.
+            var plusIndex = informationalVersion.IndexOf('+');
+            var displayVersion = plusIndex >= 0
+                ? informationalVersion[..plusIndex]
+                : informationalVersion;
+            VersionTextBlock.Text = $"Version {displayVersion}";
+        }
     }
 }
